30e2003d51769fb57d5b5da2779d940a3a1864b1
Innocent1312 <anthonydev33@gmail.com>
2020-01-24 20:12:37 +0200

    styles added

--- src/components/Projects/ProjectSummary.js
+++ src/components/Projects/ProjectSummary.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectSummary = ({ title, name, date }) => {
 
   return (
-    <section className="project-list section">
+    <section className="project-summary section">
       <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
           <h3 className="card-title">{title}</h3>
